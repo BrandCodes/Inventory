87a0130d2fc5c031685cb95c5bd3c56a32c6f72e
Brandon López <bralf_project-01@outlook.es>
2022-05-15 13:29:16 -0500

    Se comenta configuracion de carga para el model de bd

--- DataAccess/InventaryContext.cs
+++ DataAccess/InventaryContext.cs
@@ -23,28 +23,28 @@ namespace DataAccess
             }
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<CategoryEntity>().HasData(
-                new CategoryEntity { CategoryId = "ASH", CategoryName = "Aseo Hogar" },
-                new CategoryEntity { CategoryId = "ASP", CategoryName = "Aseo Personal" },
-                new CategoryEntity { CategoryId = "HGR", CategoryName = "Hogar" },
-                new CategoryEntity { CategoryId = "PRF", CategoryName = "Perfumería" },
-                new CategoryEntity { CategoryId = "SLD", CategoryName = "Salud" },
-                new CategoryEntity { CategoryId = "VDJ", CategoryName = "Video Juegos" }
-                );
+        //    modelBuilder.Entity<CategoryEntity>().HasData(
+        //        new CategoryEntity { CategoryId = "ASH", CategoryName = "Aseo Hogar" },
+        //        new CategoryEntity { CategoryId = "ASP", CategoryName = "Aseo Personal" },
+        //        new CategoryEntity { CategoryId = "HGR", CategoryName = "Hogar" },
+        //        new CategoryEntity { CategoryId = "PRF", CategoryName = "Perfumería" },
+        //        new CategoryEntity { CategoryId = "SLD", CategoryName = "Salud" },
+        //        new CategoryEntity { CategoryId = "VDJ", CategoryName = "Video Juegos" }
+        //        );
 
-            modelBuilder.Entity<WarehouseEntity>().HasData(
-                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Central", WarehouseAddress = "4627 Rowes Lane. Louisville, Ky" },
-                new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Norte", WarehouseAddress = "4085 North Street. Salt Lake City, Utah" }
-                );
+        //    modelBuilder.Entity<WarehouseEntity>().HasData(
+        //        new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Central", WarehouseAddress = "4627 Rowes Lane. Louisville, Ky" },
+        //        new WarehouseEntity { WarehouseId = Guid.NewGuid().ToString(), WarehouseName = "Bodega Norte", WarehouseAddress = "4085 North Street. Salt Lake City, Utah" }
+        //        );
 
-            modelBuilder.Entity<ProductEntity>().HasData(
-                new ProductEntity { ProductId = "ASJ-98745", ProductName = "Crema para manos marca Tersa", ProductDescription = "", CategoryId = "PRF" },
-                new ProductEntity { ProductId = "RPT-5465879", ProductName = "Pastillas para la garganta LESUS", ProductDescription = "", CategoryId = "SLD" }
-                );
-        }
+        //    modelBuilder.Entity<ProductEntity>().HasData(
+        //        new ProductEntity { ProductId = "ASJ-98745", ProductName = "Crema para manos marca Tersa", ProductDescription = "", CategoryId = "PRF" },
+        //        new ProductEntity { ProductId = "RPT-5465879", ProductName = "Pastillas para la garganta LESUS", ProductDescription = "", CategoryId = "SLD" }
+        //        );
+        //}
     }
 }
